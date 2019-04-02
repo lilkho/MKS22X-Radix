d4a48e31ece995c188fe19a920ae77f994689b16
lilkho <mkhondker@stuy.edu>
2019-04-02 14:46:53 -0400

    trying to start radix now

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,10 +1,10 @@
 public class MyLinkedList<E> {
   //NODE CLASS
   public class Node{
-    private Integer data;
+    private E data;
     private Node next,prev;
 
-    public Node(int data) {
+    public Node(E data) {
       this.data = data;
     }
 
@@ -14,7 +14,7 @@ public class MyLinkedList<E> {
     public Node prev() {
       return prev;
     }
-    public Integer getData() {
+    public E getData() {
       return data;
     }
     public void setNext(Node next) {
@@ -23,8 +23,8 @@ public class MyLinkedList<E> {
     public void setPrev(Node prev) {
       this.prev = prev;
     }
-    public Integer setData(int data) {
-      Integer orig = this.data;
+    public E setData(E data) {
+      E orig = this.data;
       this.data = data;
       return orig;
     }
@@ -52,12 +52,85 @@ public class MyLinkedList<E> {
   public void clear() {
     size=0;
   }
+  public int size() {
+    return size;
+  }
   public boolean add(E element) {
+    if (size==0) {
+      Node val = new Node(element);
+      start = val;
+      end = val;
+    } else {
+      Node val = new Node(element);
+      val.setPrev(end);
+      end.setNext(val);
+      end = val;
+    }
+    size++;
     return true;
   }
-  public void add(int index, E element) {}
-  public void extend(MyLinkedList<E> other) {}
+  public void add(int index, E element) {
+    if (index<0 || index>=size) throw new IndexOutOfBoundsException("invalid index");
+    if (index==size) add(element);
+    else {
+      Node current = getNthNode(index);
+      Node val = new Node(element);
+      val.setPrev(current.prev());
+      val.setNext(current);
+      current.prev().setNext(val);
+      current.setPrev(val);
+      size++;
+    }
+  }
+  public void extend(MyLinkedList<E> other) {
+    int otherSize = other.size();
+    Node otherStart = other.getNthNode(0);
+    otherStart.setPrev(end);
+    end.setNext(otherStart);
+    end = other.getNthNode(otherSize-1);
+    other.clear();
+    size+=otherSize;
+  }
   public E removeFront() {
-    return;
+    E orig = start.getData();
+    start = start.next;
+    return orig;
+  }
+  public E remove(int index) {
+    if (index<0 || index>=size) throw new IndexOutOfBoundsException("invalid index");
+    E orig = getNthNode(index).getData();
+    if (index==0) start=start.next();
+    else if (index==size) end=end.prev();
+    else {
+      Node current = getNthNode(index);
+      current.prev().setNext(current.next());
+      current.next().setPrev(current.prev());
+    }
+    size--;
+    return orig;
+  }
+  private Node getNthNode(int n) {
+    Node current = start;
+    for (int i=0;i<n;i++) {
+      current = current.next();
+    }
+    return current;
+  }
+
+  public static void main(String[] args) {
+    MyLinkedList<Integer> m = new MyLinkedList<Integer>();
+    System.out.println(m.add(2));
+    System.out.println(m.add(3));
+    System.out.println(m);
+    System.out.println(m.removeFront());
+    System.out.println(m);
+    m.clear();
+    System.out.println(m);
+    System.out.println(m.add(4));
+    System.out.println(m.add(5));
+    System.out.println(m.add(6));
+    m.add(1,7);
+    m.remove(1);
+    System.out.println(m);
   }
 }
